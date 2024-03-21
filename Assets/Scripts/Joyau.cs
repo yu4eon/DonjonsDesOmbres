59219c6536e7217ae3944502671d0c7ec078b440
Leon Yu <2252578@cstj.qc.ca>
2024-03-21 12:25:27 -0400

    Ajustement d'animation de rétroaction pour que ca monte, plutot que de tourner. Préparation pour fusionner les script avec ceux de l'autre programmeur

--- Assets/Scripts/Joyau.cs
+++ Assets/Scripts/Joyau.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Joyau : MonoBehaviour
 {
-    [SerializeField, Range(0, 20)] float _valeur = 5; // Valeur du joyau
+    [SerializeField, Range(0f, 1f)] float _valeur = 0.1f; // Valeur du joyau
     [SerializeField] Retroaction _retroModele;
     // [SerializeField] SOPerso _donneesPerso;
 
@@ -22,8 +22,9 @@ public class Joyau : MonoBehaviour
         {
             Niveau.instance.LibererUnePos(transform.position);
             Retroaction retro = Instantiate(_retroModele, transform.position, Quaternion.identity, transform.parent);
-            retro.ChangerTexte("+" + _valeur);
+            retro.ChangerTexte("+" + _valeur + " or");
             // _donneesPerso.argent += _valeur; //A ajouter quand on fusionne nos scripts
+            // _donneesPerso.AjouterArgent(_valeur); //Alternatif meilleur
             Destroy(gameObject);
         }
     }

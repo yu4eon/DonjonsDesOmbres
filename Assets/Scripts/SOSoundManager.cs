7b8ea27d43b11ef2a457129f95cbbab4b2e3a20b
Antoine Lachance <AntoineLachance07@hotmail.com>
2024-04-13 15:28:38 -0400

    Ajout d'un script SoundManager

---
+++ Assets/Scripts/SOSoundManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SOSoundManager", menuName = "Audio/SoundManager")]
+
+public class SOSoundManager : ScriptableObject
+{
+    
+}

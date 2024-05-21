530f0373e13f0fffea2ac0c6a11ead9913eeef64
Antoine Lachance <AntoineLachance07@hotmail.com>
2024-05-20 20:19:22 -0400

    Reglement du bug de déplacement de l'ennemiA et mise du son d'achat dans la boutique

--- Assets/Scripts/EnnemiA.cs
+++ Assets/Scripts/EnnemiA.cs
@@ -11,57 +11,48 @@ public class EnnemiA : Ennemi
     bool deplacementDroite = true;
     bool estEnTrainDeChangerDirection = false;
     Rigidbody2D rb;
+    SpriteRenderer sr;
 
     new void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(DeplacerEnnemiRoutine());
+        sr = GetComponent<SpriteRenderer>();
+        sr.flipX = true;
     }
 
-    IEnumerator DeplacerEnnemiRoutine()
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
     {
-        while (true)
-        {
-            DeplacerEnnemi();
-            yield return null;
-        }
+        DeplacerEnnemi();
     }
 
     void DeplacerEnnemi()
     {
-        if (estEnTrainDeChangerDirection)
-        {
-            return; // Si en train de changer de direction, ne fait rien
-        }
-
         GestAudio.instance.JouerEffetSonore(sonEnnemi[0]);
         Vector2 direction = deplacementDroite ? Vector2.right : Vector2.left;
         Vector2 raycastOrigin = transform.position + Vector3.down * 0.5f;
 
         RaycastHit2D hitWall = Physics2D.Raycast(raycastOrigin, direction, detectionDistance, obstacleLayer);
-        RaycastHit2D hitGround = Physics2D.Raycast(raycastOrigin, new Vector2(1,1), detectionDistance, groundLayer);
-
-        if (hitWall || !hitGround)
+        RaycastHit2D hitGround = Physics2D.Raycast(raycastOrigin, new Vector2(1,-1), detectionDistance, groundLayer);
+        if (hitWall || !hitGround || rb.velocity == Vector2.zero)
         {
-            rb.velocity = Vector2.zero;
-            if (!estEnTrainDeChangerDirection)
+            if (estEnTrainDeChangerDirection == false)
             {
-                StartCoroutine(ChangerDirectionCoroutine());
+                sr.flipX =!sr.flipX;
+                deplacementDroite =!deplacementDroite;
+                StartCoroutine(ChangerDirectionCoroutine());   
             }
         }
-
         rb.velocity = direction * vitesse;
     }
 
     IEnumerator ChangerDirectionCoroutine()
     {
-        deplacementDroite = !deplacementDroite;
-        Vector3 nouvelleEchelle = transform.localScale;
-        nouvelleEchelle.x *= -1;
-        transform.localScale = nouvelleEchelle;
         estEnTrainDeChangerDirection = true;
-
-        yield return new WaitForSeconds(4f); // Attend un court instant avant de réinitialiser la variable
+        yield return new WaitForSeconds(2f); // Attend un court instant avant de réinitialiser la variable
         estEnTrainDeChangerDirection = false;
+        yield return null;
     }
 }
